c0830e5a495ef4881bf8f4b6e2ff4ba5ab48c20c
moniceka99 <moniceka99@gmail.com>
2026-05-16 23:49:40 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
